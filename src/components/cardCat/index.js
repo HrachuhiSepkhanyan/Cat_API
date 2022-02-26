ed14949d558aa8b112c45ac1684573a4cb5584ab
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2022-02-26 21:28:29 +0400

    get images

--- src/components/cardCat/index.js
+++ src/components/cardCat/index.js
@@ -4,8 +4,8 @@ const CatsCard = ({ cat }) => {
   return (
     <div>
       <div>
-        <img src={cat.url} alt="img" width="300px" padding="20px" />
-      </div>
+        <img src={cat.url} alt="img" width="300px" />
+      </div>{" "}
     </div>
   );
 };
